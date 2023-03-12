0ea8d0ff15c5a2d27835699f08704ef27fd53df9
Matias Wengiel <matiaswengiel@gmail.com>
2023-03-12 17:26:53 -0500

    Fix CORS issue on server, fine-tune start script

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/api", (req, res) => {
   res.send("Hello, I am the home page");
 });
 
